fffbe57e5f849c7409a59acdb50bd0cc909faf00
kstep99 <kstep99@outlook.com>
2023-11-05 02:44:26 -0600

    step two completed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,18 +1,22 @@
 class Customer < ApplicationRecord
-  # Devise modules
+  # Includes Devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Override Devise's email required method
-  def email_required?
-    false
-  end
-
 
   # Active Storage association for the image
   has_one_attached :image
 
-  # Only allow certain attributes to be searchable
+  # Validations
+  validates :full_name, presence: true
+  # Ensures the email is unique and allows it to be blank (if your application logic requires it)
+  validates :email, uniqueness: true, allow_blank: true
+
+  # Add phone number format validation (the regex can be adjusted as needed)
+  validates :phone_number, format: { with: /\A(\+\d{1,3}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/ }, allow_blank: true
+
+  # Ransackable Attributes and Associations
+  # Only allow certain attributes to be searchable.
   def self.ransackable_attributes(auth_object = nil)
     %w[id full_name email phone_number created_at updated_at]
   end
@@ -20,17 +24,6 @@ class Customer < ApplicationRecord
   # Allowlist associations for Ransack searches
   # Return an empty array if you do not want any associations to be searchable
   def self.ransackable_associations(auth_object = nil)
-    # Example: %w[orders payments]
     []
   end
-
-  # Validations
-  validates :full_name, presence: true
-
-  # You might have a typo here, it should be :email if that's the column in your database, not :email_address
-  # So either change your validation to match the database column, or change the database column to match this (which may involve a migration).
-  validates :email, uniqueness: true, allow_blank: true
-
-  # Uncomment and edit the line below to add phone number format validation as required
-  # validates :phone_number, format: { with: /\A(\+\d{1,3}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ }, allow_blank: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+   # This sets the customers index as the homepage for your app
+   root 'customers#index'
+
+   # Resourceful routes for customers, which includes routes for index, show, new, edit, create, update, and destroy actions
+   resources :customers
 end
