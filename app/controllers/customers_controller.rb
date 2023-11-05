555ee2596b5480c355b6ca266462f230da206f9c
kstep99 <kstep99@outlook.com>
2023-11-05 03:02:17 -0600

    step 3 part 1 complete

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,19 @@
+# app/controllers/customers_controller.rb
+
 class CustomersController < ApplicationController
+  # Updated index action to retrieve alphabetized customers
   def index
-    @customers = Customer.all
+    @alphabetized_customers = Customer.order(:full_name)
   end
 
+  # Show action to find a customer by id
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  # You may want to remove the alphabetized action if it's no longer needed
+  # or leave it if you plan to use it for a separate alphabetized view.
+  def alphabetized
+    @alphabetized_customers = Customer.order(:full_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :customers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-   # This sets the customers index as the homepage for your app
-   root 'customers#index'
+  # This sets the customers index as the homepage for your app
+  root 'customers#index'
 
-   # Resourceful routes for customers, which includes routes for index, show, new, edit, create, update, and destroy actions
-   resources :customers
+  # Resourceful routes for customers
+  resources :customers do
+    collection do
+      get 'alphabetized' # This adds the /customers/alphabetized path
+    end
+  end
 end
