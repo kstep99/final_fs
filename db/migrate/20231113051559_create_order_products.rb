c38ff32084b3bb7c90a5f1c5d7505c9ec87edc39
kstep99 <kstep99@outlook.com>
2023-11-12 23:31:05 -0600

    initial db made, basic admin and user login made, with flaws

--- db/migrate/20231113051559_create_order_products.rb
+++ db/migrate/20231113051559_create_order_products.rb
@@ -1,9 +1,9 @@
 class CreateOrderProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :order_products do |t|
-      t.integer :quantity
-      t.decimal :price
-      t.decimal :subtotal
+      t.integer :quantity, null: false, default: 1
+      t.decimal :price, precision: 10, scale: 2, null: false
+      t.decimal :subtotal, precision: 12, scale: 2, null: false
       t.references :order, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
 
