708293ea1b6cd6fdae765ad5365f4f1e4624c1c8
kstep99 <kstep99@outlook.com>
2023-11-19 03:34:19 -0600

    testing git macro

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,16 @@
 # app/controllers/products_controller.rb
 class ProductsController < ApplicationController
   def index
-    @products = if params[:category_id].present?
-                  Product.where(category_id: params[:category_id])
-                         .with_attached_images
-                         .page(params[:page]) # Kaminari's page method
-                else
-                  Product.with_attached_images.page(params[:page]) # Paginate all products
-                end
+    # Start with all products or a specific category if provided
+    @products = params[:category_id].present? ? Product.where(category_id: params[:category_id]) : Product.all
+
+    # Filter by keyword if provided
+    if params[:keyword].present?
+      @products = @products.where('name LIKE :keyword OR description LIKE :keyword', keyword: "%#{params[:keyword]}%")
+    end
+
+    # Continue with existing pagination
+    @products = @products.with_attached_images.page(params[:page])
   end
 
   def show
