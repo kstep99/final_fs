0fd86f9fc23e36cbef60d81ee48a10af79bf6949
kstep99 <kstep99@outlook.com>
2023-11-28 17:19:18 -0600

    fixed autoloading shipping data from cust db

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,30 +2,29 @@ class OrdersController < ApplicationController
   before_action :authenticate_customer!, only: %i[new create]
 
   def new
-    # existing code
+    @customer = current_customer
+
   end
 
   def calculate_taxes
     province_id = params[:province_id]
     province = Province.find(province_id)
 
-    tax_amount, total_price = calculate_order_taxes(province)
+    subtotal = calculate_cart_subtotal
+    tax_amount, total_price = calculate_order_taxes(subtotal, province)
 
-    render json: { tax_amount:, total_price: }
+    render json: { tax_amount: tax_amount, total_price: total_price }
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Province not found' }, status: :not_found
   end
 
   private
 
-  def calculate_order_taxes(province)
-    subtotal = calculate_cart_subtotal
-
+  def calculate_order_taxes(subtotal, province)
     gst_amount = calculate_tax(subtotal, province.gst)
-    pst_amount = calculate_tax(subtotal, province.pst)
-    hst_amount = calculate_tax(subtotal, province.hst)
+    pst_or_hst_amount = province.hst.present? ? calculate_tax(subtotal, province.hst) : calculate_tax(subtotal, province.pst)
 
-    total_tax = hst_amount > 0 ? hst_amount : (gst_amount + pst_amount)
+    total_tax = gst_amount + pst_or_hst_amount
     total_price = subtotal + total_tax
 
     [total_tax, total_price]
--- app/javascript/packs/checkout.js
+++ app/javascript/packs/checkout.js
@@ -1,5 +1,12 @@
 console.log('checkout.js loaded');
 
+// Constants for shipping costs
+const SHIPPING_COSTS = {
+  purolator: 10.0, // Example cost
+  canada_post: 8.0,
+  dhl: 12.0
+};
+
 function updateTaxInfo() {
   let provinceId = $('#order_province_id').val();
   console.log('Province ID:', provinceId);
@@ -20,11 +27,13 @@ function updateTaxInfo() {
       console.log("Tax Response:", response);
 
       let totalTax = parseFloat(response.tax_amount) || 0;
-      let total = subtotal + totalTax;
+      let shippingCost = $('input[name="shipping_option"]:checked').val() ? SHIPPING_COSTS[$('input[name="shipping_option"]:checked').val()] : 0;
+      let total = subtotal + totalTax + shippingCost;
 
-      console.log('Total Tax:', totalTax, 'Total:', total);
+      console.log('Total Tax:', totalTax, 'Shipping Cost:', shippingCost, 'Total:', total);
 
       $('#tax_amount').text(`$${totalTax.toFixed(2)}`);
+      $('#shipping_cost').text(`$${shippingCost.toFixed(2)}`);
       $('#total_price').text(`$${total.toFixed(2)}`);
     },
     error: function(error) {
@@ -32,6 +41,12 @@ function updateTaxInfo() {
     }
   });
 }
+
+// Add event listeners for shipping option changes
+$('input[name="shipping_option"]').on('change', function() {
+  console.log('Shipping option changed');
+  updateTaxInfo();
+});
 
 $(document).on('change', '#order_province_id', function() {
   console.log('Province dropdown changed');
