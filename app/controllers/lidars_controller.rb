fca48b70bbe4e5b9b1cb13efa9ec4819e4c35344
kstep99 <kstep99@outlook.com>
2023-12-11 02:43:17 -0600

    potree window opens, 404 on cloud.js, very close now

--- app/controllers/lidars_controller.rb
+++ app/controllers/lidars_controller.rb
@@ -5,4 +5,15 @@ class LidarsController < ApplicationController
     @lidar_files = current_customer.lidar_files
   end
 
+  def viewer
+    file_name = params[:file_name]
+    file_path = Rails.root.join('public', 'potree', "#{file_name}.html")
+
+    if File.exist?(file_path)
+      render file: file_path, layout: false
+    else
+      # Handle the case where the file does not exist
+      render plain: "File not found", status: :not_found
+    end
+  end
 end
